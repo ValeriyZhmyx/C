2b88fbb7d4b8f242cc69e9925c16492f5a6532d4
Енотик-полоскун <97868095+ValeriyZhmyx@users.noreply.github.com>
2023-05-12 11:09:08 +0700

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Unit u1 = new Unit("Воин", 22, 200);
+            Unit u2 = new Unit("Бес", 20, 200);
+
+            while (true)
+            {
+                if (u1.Attack(u2) == false || u2.Attack(u1) == false) break;
+            };
+
+
+            u1.Display();
+            u2.Display();
+
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Unit.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    class Unit
+    {
+        private String name;
+        private int damage;
+        private int health;
+        
+        public Unit(string n, int d, int h)
+        {
+            name = n;
+            damage = d;
+            health = h;
+
+        }
+
+        public bool Attack(Unit enemy)
+        {
+        if (enemy.health <= 0 || health <= 0) return false;
+                System.Console.Write(name + " атакует " + enemy.name);
+                System.Console.Write(" и наносит " + damage + " урона\n");
+                enemy.health -= damage;
+        return true;
+        }
+        public void Display()
+        {
+            Console.WriteLine("Имя: " + name);
+            Console.WriteLine("Урон: " + damage);
+            Console.WriteLine("Здоровье: " + health);
+        }
+    }
